9d97c52c13c0557a0a85c1c5988ac700971ee5e9
KishoreBaktha <kishorebaktha@gmail.com>
2017-10-04 17:42:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kishorebaktha/websitetoandroidapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.kishorebaktha.websitetoandroidapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView=(WebView)findViewById(R.id.web);
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://internshala.com/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+    }
+}
